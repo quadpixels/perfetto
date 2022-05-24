c1c71025e9bf5c22951710f384f8efec2422bbbd
nitroglycerine <suichen6@gmail.com>
2022-05-24 02:05:27 -0700

    Very naive OpenBMC SDK support
    This change allows one to build OpenBMC-compatible Perfetto binaries
    using an OpenBMC SDK.
    
    This change hard-codes support for evb-ast2500.
    
    Usage: (Assuming OpenBMC SDK is installed in ~/oecore-x86_64)
    $ tools/build_all_configs.py --openbmc --openbmc-sdk ~/oecore-x86_64
    $ cd out/openbmc_gcc_release_arm/
    $ ninja -C .
    [.../2475] compile ...
    
    TODO: find a way to determine architecture

--- tools/build_all_configs.py
+++ tools/build_all_configs.py
@@ -22,6 +22,7 @@ import os
 import platform
 import subprocess
 import sys
+import re
 
 from compat import iteritems, quote
 
@@ -62,10 +63,77 @@ LINUX_BUILD_CONFIGS = {
                      'is_asan=true'),
 }
 
-LINUX_ARCHS = ('arm64',)
+LINUX_ARCHS = ('arm', 'arm64',)
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+OPENBMC_BUILD_CONFIGS = {
+    'openbmc_gcc_debug':   ('is_clang=false', 'is_debug=true' ),
+    'openbmc_gcc_release': ('is_clang=false', 'is_debug=false'),
+}
+
+OPENBMC_ARCHS = ('arm',)
+
+def GetOpenBMCVariables(sdkpath):
+  ret = {}
+  maybe_sr = os.path.join(sdkpath, "sysroots")
+  if os.path.exists(maybe_sr):
+    for p in os.listdir(maybe_sr):
+      if p.endswith("gnueabi"):
+        ret["sysroot"] = os.path.join(maybe_sr, p)
+  filenames = [os.path.join(dp, f) for dp, dn, fn in \
+    os.walk(os.path.expanduser(sdkpath)) for f in fn]
+  for fn in filenames:
+    fn = fn.strip()
+    if fn.endswith("gnueabi-g++"):
+      ret["g++"] = fn
+    elif fn.endswith("gnueabi-gcc-ar"):
+      ret["ar"] = fn
+    elif fn.endswith("gnueabi-gcc"):
+      ret["gcc"] = fn
+    elif fn.endswith("gnueabi-strip"):
+      ret["strip"] = fn
+  return ret
+
+def ProcessOpenBMCToolchainNinjaFile(file_path, openbmc_vars):
+  with open(file_path) as f:
+    lines = f.readlines()
+    for i in range(len(lines)):
+      lines[i] = re.sub("--sysroot=[\S]+",
+                        "--sysroot=" + openbmc_vars["sysroot"],
+                        lines[i])
+      lines[i] = lines[i].replace("arm-linux-gnueabihf-g++",
+                                  openbmc_vars["g++"])
+      lines[i] = lines[i].replace("arm-linux-gnueabihf-gcc",
+                                  openbmc_vars["gcc"])
+      lines[i] = lines[i].replace("arm-linux-gnueabihf-ar",
+                                  openbmc_vars["ar"])
+      lines[i] = lines[i].replace(" strip ",
+                                  " " + openbmc_vars["strip"] + " ")
+  with open(file_path, 'w') as f:
+    for line in lines:
+      f.write(line)
+
+def RemoveICFAllFlag(out_dir):
+  filenames = [os.path.join(dp, f) for dp, dn, fn in \
+    os.walk(os.path.expanduser(out_dir)) for f in fn]
+  for fn in filenames:
+    if fn.endswith(".ninja"):
+      with open(fn) as f:
+        lines = f.readlines()
+      replaced = False
+      for i in range(len(lines)):
+        for x in ["-Wl,--icf=all", "-Werror", "-mfpu=neon", "-mthumb"]:
+          if x in lines[i]:
+            lines[i] = lines[i].replace(x, "")
+            replaced = True
+        if "-march=armv7-a" in lines[i]:
+          lines[i] = lines[i].replace("-march=armv7-a",
+                                      "-marm -mcpu=arm1176jz-s") # TODO
+          replaced = True
+      if replaced:
+        with open(fn, "w") as f:
+          for line in lines: f.write(line)
 
 def main():
   parser = argparse.ArgumentParser()
@@ -75,26 +143,46 @@ def main():
   parser.add_argument(
       '--export-compile-commands', action="store_true", default=False)
   parser.add_argument('--build', metavar='TARGET')
+  parser.add_argument('--openbmc', action='store_true', default=False)
+  parser.add_argument('--openbmc-sdk', metavar='OPENBMC_SDK')
   args = parser.parse_args()
+  print(args.openbmc_sdk)
 
   configs = {}
   if not args.host_only:
+    if args.openbmc:
+      print("OpenBMC build")
     if args.android:
       for config_name, gn_args in iteritems(ANDROID_BUILD_CONFIGS):
         for arch in ANDROID_ARCHS:
           full_config_name = '%s_%s' % (config_name, arch)
           configs[full_config_name] = gn_args + ('target_cpu="%s"' % arch,)
-    for config_name, gn_args in iteritems(LINUX_BUILD_CONFIGS):
-      if dict(a.split('=') for a in gn_args).get('is_clang', None) == 'true':
-        continue
-      for arch in LINUX_ARCHS:
-        full_config_name = '%s_%s' % (config_name, arch)
-        configs[full_config_name] = gn_args + ('target_cpu="%s"' % arch,
-                                               'target_os="linux"')
+    if args.openbmc:
+      if args.openbmc_sdk is None:
+        print("Please specify OpenBMC SDK location using '--openbmc-sdk SDK_PATH"
+              "' when building for OpenBMC.")
+        exit(0)
+      openbmc_vars = GetOpenBMCVariables(args.openbmc_sdk)
+      if len(openbmc_vars) < 5:
+        print("The specified SDK path does not look valid.")
+        exit(0)
+      for config_name, gn_args in iteritems(OPENBMC_BUILD_CONFIGS):
+        for arch in OPENBMC_ARCHS:
+          full_config_name = '%s_%s' % (config_name, arch)
+          configs[full_config_name] = gn_args + ('target_cpu="%s"' % arch,)
+    else:
+      for config_name, gn_args in iteritems(LINUX_BUILD_CONFIGS):
+        if dict(a.split('=') for a in gn_args).get('is_clang', None) == 'true':
+          continue
+        for arch in LINUX_ARCHS:
+          full_config_name = '%s_%s' % (config_name, arch)
+          configs[full_config_name] = gn_args + ('target_cpu="%s"' % arch,
+                                                 'target_os="linux"')
 
   system = platform.system().lower()
   if system == 'linux':
-    configs.update(LINUX_BUILD_CONFIGS)
+    if args.openbmc == False:
+      configs.update(LINUX_BUILD_CONFIGS)
   elif system == 'darwin':
     configs.update(MAC_BUILD_CONFIGS)
   else:
@@ -126,6 +214,11 @@ def main():
       ninja_cmd = (ninja, '-C', '.', args.build)
       subprocess.check_call(ninja_cmd, cwd=out_dir)
 
+    if args.openbmc:
+      print("Post-proessing OpenBMC build configuration")
+      ProcessOpenBMCToolchainNinjaFile(os.path.join(out_dir, "toolchain.ninja"),
+                                       openbmc_vars)
+      RemoveICFAllFlag(out_dir)
 
 if __name__ == '__main__':
   sys.exit(main())
